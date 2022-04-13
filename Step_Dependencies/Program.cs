07ab845d913b21936f63391706ed80faa9f4ad33
Cortlynd101 <CortlyndCox@gmail.com>
2022-04-13 15:20:13 -0600

    Added the class processes.

--- Step_Dependencies/Program.cs
+++ Step_Dependencies/Program.cs
@@ -7,7 +7,7 @@ namespace Step_Dependencies
     {
         static void Main(string[] args)
         {
-            string item = @"C:\Users\Cortl\Source\Repos\step-dependencies-Cortlynd101\Step_Dependencies\PracticeFile.txt";
+            string item = @"C:\Users\Cortl\Source\Repos\step-dependencies-Cortlynd101\Step_Dependencies\FinalInput.txt";
             StepDependency steps = new StepDependency(item);
 
             for (int i = 0; i < steps.stepsList.Count; i++)
@@ -18,12 +18,12 @@ namespace Step_Dependencies
             Console.WriteLine("The correct order is: ");
             for (int i = 0; i < steps.orderOfNonDependent.Length; i++)
             {
-                if (i < steps.orderOfNonDependent.Length - 3)
+                if (i < 25)
                 {
                     //Console.WriteLine(steps.orderOfNonDependent.Length);
                     Console.Write(steps.orderOfNonDependent[i] + " --> ");
                 }
-                else if (i < steps.orderOfNonDependent.Length - 2)
+                else if (i < 26)
                 {
                     Console.Write(steps.orderOfNonDependent[i]);
                 }
